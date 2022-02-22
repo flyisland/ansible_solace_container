b67c3dd42fdcf4abdb003355e64414aaf8ce9481
flyisland <flyisland@gmail.com>
2022-02-22 19:40:48 +0800

    feat: create py script to generate docker create command line for each node of a HA triple

---
+++ ha_docker_scripts.py
@@ -0,0 +1,36 @@
+import os
+import configparser
+from jinja2 import Environment, FileSystemLoader
+
+
+def main():
+    cp = configparser.ConfigParser()
+    cp.read("./hosts")
+    all_vars = cp["all:vars"]
+    vars = {
+        "scale_tier": all_vars["scale_tier"],
+        "router_name_suffix": all_vars["router_name_suffix"],
+        "primary_ip": all_vars["primary_ip"],
+        "backup_ip": all_vars["backup_ip"],
+        "monitoring_ip": all_vars["monitoring_ip"],
+        "vmr_version": {"stdout": "solace/solace-pubsub-standard"},
+    }
+
+    filename = "./roles/ha/templates/ha-create.sh.j2"
+    filedir = os.path.dirname(os.path.abspath(filename))
+    filename = os.path.basename(filename)
+    e = Environment(
+        loader=FileSystemLoader(filedir), trim_blocks=True, lstrip_blocks=True
+    )
+    template = e.get_template(filename)
+
+    roles = ["primary", "backup", "monitor"]
+    for role in roles:
+        vars["ha_role"] = role
+        docker_script = template.render(vars)
+        print("{:-^80}".format(role))
+        print(docker_script)
+
+
+if __name__ == "__main__":
+    main()
